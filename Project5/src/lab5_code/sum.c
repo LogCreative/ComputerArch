2c5664e79e609b7f09e878737e912675b0a9a49e
LogCreative <longlaoda123456@qq.com>
2021-05-22 13:53:19 +0800

    lab05

--- Project5/src/lab5_code/sum.c
+++ Project5/src/lab5_code/sum.c
@@ -13,16 +13,16 @@
 
 #define CLOCK_RATE_GHZ 2.26e9
 
-// static __inline__ uint64_t RDTSC()
-// {
-//     uint32_t hi, lo;
-//     __asm__ volatile
-// 	(
-// 		"rdtsc"
-// 		: "=a"(lo), "=d"(hi)
-// 	);
-// 	return (((uint64_t)hi) << 32) | ((uint64_t)lo);
-// }
+static __inline__ uint64_t RDTSC()
+{
+    uint32_t hi, lo;
+    __asm__ volatile
+	(
+		"rdtsc"
+		: "=a"(lo), "=d"(hi)
+	);
+	return (((uint64_t)hi) << 32) | ((uint64_t)lo);
+}
 
 static int sum_naive(int n, int *a)
 {
@@ -60,24 +60,20 @@ static int sum_unrolled(int n, int *a)
 static int sum_vectorized(int n, int *a)
 {
     // WRITE YOUR VECTORIZED CODE HERE
-	
-	// To increase the robustness
-	// of this program, sizeof is used to
-	// determine the number of operations.
-	// 128 bit = 16 bytes
 
-	int steps = 16 / sizeof(int);
+	// the parallelization could only
+	// apply on aligned groups
+	int groups =  n / 4;
 
 	// On linux, int is 32-bit wide.
 	// 128-bit = 4 int;
-	
 	// Initialize sum vector of 128-bit as zero.
 	__m128i sum = _mm_setzero_si128();
 
 	// For loop will add numbers on mod 4 basis.
-	for(int i = 0; i < n / steps; ++i){
+	for(int i = 0; i < groups; ++i){
 		// load a vector of adders.
-		__m128i adder = _mm_loadu_si128(a + i * steps);
+		__m128i adder = _mm_loadu_si128(a + i * 4);
 
 		// add the vector to a temporary variable.
 		__m128i added = _mm_add_epi32(sum, adder);
@@ -86,22 +82,52 @@ static int sum_vectorized(int n, int *a)
 		_mm_storeu_si128(&sum, added);
 	}
 
+	int result = 0;
+
 	// However there is some remaining numbers
 	// that didn't count, whose quantity is < 4.
+	int remain = n - groups * 4;
+	int tip = groups * 4;
+
+	// For each offset, calulate manually.
+	for(int j = 0; j < remain; ++j)
+		result += a[tip++];
+
 	// and to get the final result, the member
 	// of the vector in sum has to be added as well,
 	// in other word, 4 numbers.
-
 	
+	// For each right shift, get the lower 32 bit.
+	// which is one unit and add to the result.
+	for(int i = 0; i < 4; ++i)
+		result += _mm_cvtsi128_si32(_mm_srli_si128(sum,i * 4));
 
-    return 0;
+    return result;
 }
 
 static int sum_vectorized_unrolled(int n, int *a)
 {
     // UNROLL YOUR VECTORIZED CODE HERE
 
-     return 0;
+	__m128i sum = _mm_setzero_si128();
+
+	// For unrolled scenario,
+	// every loop will contribute 128 * 4 = 512 bit operation.
+	for(int i = 0; i < n / 16 * 16; i += 16){
+		_mm_storeu_si128(&sum, _mm_add_epi32(sum, _mm_loadu_si128(a + i )));
+		_mm_storeu_si128(&sum, _mm_add_epi32(sum, _mm_loadu_si128(a + i + 4)));
+		_mm_storeu_si128(&sum, _mm_add_epi32(sum, _mm_loadu_si128(a + i + 8)));
+		_mm_storeu_si128(&sum, _mm_add_epi32(sum, _mm_loadu_si128(a + i + 12)));
+	}
+	
+	int result = 0;
+	for(int i = n / 16 * 16; i < n; i++)
+		result += a[i];
+	
+	for(int k = 0; k < 4; ++k)
+		result += _mm_cvtsi128_si32(_mm_srli_si128(sum,k * 4));
+
+    return result;
 }
 
 void benchmark(int n, int *a, int(*computeSum)(int, int*), char *name)
